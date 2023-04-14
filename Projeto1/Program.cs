56d998c71ea3123af403c8895a5fec39485003a9
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-04-14 22:58:04 +0100

    Creation of the buttons that control the lamps

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -2,11 +2,18 @@
 
 namespace Projeto1
 {
+    
     class Program
     {
-        static void Main(string[] args)
+        private void create_buttons (object sender, EventArgs e)
         {
-            Console.WriteLine("Hello, World!");
+            //Creates the button for the first lamp
+              button1 = new Button();
+              button1.Location = new System.Drawing.Point(15, 15);
+              button1.Size = new.System.Drawing.Size(90, 30);
+              button1.Label = "Lamp 1";
+              button1.Press += System.EventHandler(button1.Press);
+              this.Controls.Add(button1);
         }
     }
 }
