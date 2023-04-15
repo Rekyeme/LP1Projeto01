32d7c24de87ae435dc7ea787e0351bbb0ad7ad74
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-04-15 20:00:16 +0100

    Added instructions and unicode char

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 
+
 namespace Projeto1
 {
     
@@ -17,6 +18,10 @@ namespace Projeto1
 
         static void Main(string[] args)
         {
+           
+            Instructions();
+            Console.WriteLine($"Initial state:");
+            LampState();
 
             while (Globals.i<6)
             {   
@@ -37,9 +42,9 @@ namespace Projeto1
 
                 
             }
+
+            Console.ResetColor();
         }
-
-
         static void ChangeLamp(int a)
         {
 
@@ -54,16 +59,39 @@ namespace Projeto1
             }
             
             Globals.i++;
-            Text();
+            LampState();
         }
 
-        static void Text()
+        static void LampState()
         {
             foreach (bool item in Globals.lamps)
             {
-                Console.Write($"{item} ");
+                if (item==true)
+                {
+                    Console.Write($"\t \u25CF");
+                }
+                else if (item==false)
+                {
+                    Console.Write($"\t \u25CB");
+                }
             }
-            Console.WriteLine($"");
+            Console.WriteLine($"\n");
+        }
+        static void Instructions()
+        {
+            Console.BackgroundColor=ConsoleColor.DarkCyan;
+            Console.WriteLine($"\nINSTRUCTIONS:\n");
+            Console.WriteLine($"This game has 3 lamps and 3 buttons.");
+            Console.WriteLine($"When the lamp is turned off: \u25CB");
+            Console.WriteLine($"When the lamp is turned on:  \u25CF \n");
+            Console.Write($"To press the buttons 1,2 and 3 the player ");
+            Console.WriteLine($"must press the keys 1,2 and 3, respectively");
+            Console.WriteLine($"The button 1 switches the state of the lamp 1");
+            Console.WriteLine($"The button 2 of the lamps 1 and 2");
+            Console.WriteLine($"And the button 3 of the lamps 2 and 3\n");
+            Console.Write($"To win the game the player must turn on all ");
+            Console.WriteLine($"the lamps\n");
+            
         }
        
        
