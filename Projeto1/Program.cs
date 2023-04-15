b6fadec3a80b7e2d83581e1db4ebbdeb732e38d8
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-04-15 16:49:41 +0100

    Added keys for the buttons

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -1,35 +1,72 @@
 ﻿using System;
-using System.Windows.Forms
+
 
 namespace Projeto1
 {
-    class Object
+    
+    class Program
     {
-        private void create_buttons (object sender, EventArgs e)
-        
-            //Creates the first button that changes the state of the first lamp;
-            button1 = new Button;
-            button1.Location = new System.Drawing.Point(15, 15);
-            button1.Size = new.System.Drawing.Size(90, 30);
-            button1.Label = "Lamp 1";
-            button1.Press += System.EventHandler(button1.Press);
-            this.Controls.Add(button1);
 
-            //Creates the second button that changes the state of the first and second lamp;
-            button2 = new Button();
-            button2.Location = new System.Drawing.Point(125, 15);
-            button2.Size = new.System.Drawing.Size(90,30);
-            button2.Label = "Lamp 1 and Lamp 2";
-            button2.Press += System.EventHandler(button2.Press);
-            this.Controls.Add(button2)
 
-            //Creates the third button that changes the state of the second and third lamp;
-            button3 = new Button();
-            button3.Location = new System.Drawing.Point(225, 15);
-            button3.Size = new.System.Drawing.Size(90,30);
-            button3.Label = "Lamp 2 and Lamp 3";
-            button3.Press += System.EventHandler(button3.Press);
-            this.Controls.Add(button3)
-    } 
+        public static class Globals
+        {
+            public static int i=0;
+            public static bool[] lamps={false,false,false};
+        }
+       
+
+        static void Main(string[] args)
+        {
+
+            while (Globals.i<6)
+            {   
+                ConsoleKeyInfo keys=Console.ReadKey(true);
+            
+                if (keys.Key==ConsoleKey.D1)
+                {
+                    ChangeLamp(1);
+                }
+                else if (keys.Key==ConsoleKey.D2)
+                {
+                    ChangeLamp(2);
+                }
+                else if (keys.Key==ConsoleKey.D3)
+                {
+                    ChangeLamp(3);
+                }
+
+                
+            }
+        }
+
+
+        static void ChangeLamp(int a)
+        {
+
+            if (a != 1)
+            {
+                Globals.lamps[a-1]= !Globals.lamps[a-1];
+                Globals.lamps[a-2]= !Globals.lamps[a-2];
+            }
+            else 
+            {
+                Globals.lamps[0]=!Globals.lamps[0];
+            }
+            
+            Globals.i++;
+            Text();
+        }
+
+        static void Text()
+        {
+            foreach (bool item in Globals.lamps)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine($"");
+        }
+       
+       
+    }
 }
 
