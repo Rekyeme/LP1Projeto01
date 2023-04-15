0524e312ef93a1f218d211934cff3f6d2aa99ffd
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-04-15 14:10:31 +0100

    Add Change state of 1 lamp

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -2,11 +2,38 @@
 
 namespace Projeto1
 {
+   
     class Program
     {
+
+
+        public static class Globals
+        {
+            public static int i=0;
+            public static bool[] lamps={false,false,false};
+        }
+       
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            while (Globals.i<6)
+            {
+                if ( Console.ReadKey(true).Key!=ConsoleKey.NumPad1)
+                {
+                    ChangeLamp(1);
+                }
+
+                Console.WriteLine($"{Globals.lamps[0]}");
+            }
         }
+        static void ChangeLamp(int a)
+        {
+            Globals.lamps[a-1]= !Globals.lamps[a-1];
+            Globals.i++;
+
+        }
+       
+       
     }
 }
